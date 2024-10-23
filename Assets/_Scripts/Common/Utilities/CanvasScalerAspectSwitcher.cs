3adf7c9695926feb887f77f69591fd3ffbde3cf9
Suzu <minato1932@gmail.com>
2024-10-23 14:46:58 -0300

    Add Canvas Scaler To UI

---
+++ Assets/_Scripts/Common/Utilities/CanvasScalerAspectSwitcher.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(CanvasScaler))]
+public class CanvasScalerAspectSwitcher : MonoBehaviour
+{
+    CanvasScaler canvasScaler;
+    RectTransform rectTransform;
+
+    void OnEnable() => OnRectTransformDimensionsChange();
+
+    void OnRectTransformDimensionsChange()
+    {
+        Debug.Log("is working");
+
+        if (!canvasScaler) canvasScaler = GetComponent<CanvasScaler>();
+        if (!rectTransform) rectTransform = GetComponent<RectTransform>();
+
+        var resolution = rectTransform.sizeDelta;
+        var aspect = resolution.x / resolution.y;
+
+        var min = canvasScaler.referenceResolution;
+        var minAspect = min.x / min.y;
+
+        canvasScaler.matchWidthOrHeight = aspect < minAspect ? 0 : 1;
+    }
+}
