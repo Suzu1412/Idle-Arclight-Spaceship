8c3ec92b9c6f1bed233d4f69000a5d7ce65bf2d5
Suzu <minato1932@gmail.com>
2025-02-04 21:12:26 -0300

    creating sprite atlas

--- Assets/_Scripts/Gameplay/Attack/Projectile/ProjectileDataSO.cs
+++ Assets/_Scripts/Gameplay/Attack/Projectile/ProjectileDataSO.cs
@@ -1,15 +1,18 @@
+using System;
 using UnityEngine;
+using UnityEngine.U2D;
 
 [CreateAssetMenu(fileName = "ProjectileDataSO", menuName = "Scriptable Objects/Attack/Projectile Data")]
 public class ProjectileDataSO : ScriptableObject
 {
-    [SerializeField] private Sprite _sprite;
+    [SerializeField] private SpriteAtlas _atlas;
+    [SerializeField] private string _spriteName;
     [SerializeField][Range(5, 20f)] private float _projectileLifeTime;
     [SerializeField][Range(1, 5)] private int _projectileDuration = 1;
 
     public void Initialize(Projectile projectile, Agent agent, Vector2 direction, Vector3 position, float speed)
     {
-        projectile.SetSprite(_sprite);
+        projectile.SetSprite(_atlas.GetSprite(_spriteName));
         projectile.SetLifeTime(_projectileLifeTime);
         projectile.SetProjectileSpeed(speed);
         projectile.SetLayerMask(agent.AttackSystem.ProjectileMask);
